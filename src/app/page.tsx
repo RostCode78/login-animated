9bab3514383804cc20c8cc566920d50086567cb5
RostCode78 <zeroxcode78@gmail.com>
2024-07-23 01:06:39 -0600

    First commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,104 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client';
+import { useState } from 'react';
 
 export default function Home() {
+
+  const [ showLogin, setShowLogin ] = useState(false);
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="container">
+      <div className="mobile-content">
+
+        <div className={ showLogin ? "singup-content show" : "singup-content" }>
+          <div className="background">
+            <div className="overlay-blur"/>
+            <img src="https://i.imgur.com/F49FcWO.jpg" alt="background" />
+          </div>
+
+          <div className="form-content">
+            <div className="titulo">
+              <h1>Sign up</h1>
+            </div>
+
+            <div className="data">
+
+              <form>
+                <input
+                  type="text"
+                  name="name"
+                  placeholder="Name"
+                  autoComplete="nope"
+                  // Ni pedo, no jalo el autoComplete="nope"
+                />
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="Email"
+                  autoComplete="nope"
+                />
+                <input
+                  type="password"
+                  name="password"
+                  placeholder="Password"
+                  autoComplete="nope"
+                />
+              </form>
+
+              <div className="btn-content">
+                <button className="btn-primary">
+                  <p>Sign Up</p>
+                </button>
+              </div>
+
+            </div>
+          </div>
+
+          <div className="btn-signup" onClick={ () => setShowLogin(!showLogin ) }>
+            <p>or <span>Sign up</span></p>
+          </div>
+
         </div>
+
+        <div className={ showLogin ? "login-content show" : "login-content" }>
+
+          <div className="form-content">
+            <div className="titulo">
+              <h1>Log In</h1>
+            </div>
+
+            <div className="data">
+
+              <form>
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="Email"
+                  autoComplete="nope"
+                />
+                <input
+                  type="password"
+                  name="password"
+                  placeholder="Password"
+                  autoComplete="nope"
+                />
+              </form>
+
+              <div className="btn-content">
+                <button className="btn-primary">
+                  <p>Log In</p>
+                </button>
+              </div>
+
+            </div>
+          </div>
+
+          <div className="btn-action" onClick={ () => setShowLogin(!showLogin ) }>
+            <p>or <span>Log in</span></p>
+          </div>
+
+        </div>
+
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   );
 }
